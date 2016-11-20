f32fbbe2f59729ee96d73fff8da32c907e19afc4
yarec <yarec@163.com>
2016-11-20 10:56:00 +0800

    tab color

--- browser-tabs.jsx
+++ browser-tabs.jsx
@@ -15,19 +15,23 @@ var BrowserTabs = React.createClass({
   render: function () {
     var self = this
     return <div id="browser-tabs">
-      <a className="close" onClick={this.props.onClose}><i className="fa fa-circle" /></a>
-      <a className="minimize" onClick={this.props.onMinimize}><i className="fa fa-circle" /></a>
-      <a className="maximize" onClick={this.props.onMaximize}><i className="fa fa-circle" /></a>
       {this.props.pages.map(function (page, i) {
         if (!page)
           return
-        
+
         function onClick (e) { self.props.onTabClick(e, page, i) }
         function onContextMenu (e) { self.props.onTabContextMenu(e, page, i) }
         function onClose (e) { e.preventDefault(); e.stopPropagation(); self.props.onTabClose(e, page, i) }
         return <BrowserTab key={'browser-tab-'+i} isActive={self.props.currentPageIndex == i} page={page} onClick={onClick} onContextMenu={onContextMenu} onClose={onClose} />
       })}
-      <a className="newtab" onClick={this.props.onNewTab}><i className="fa fa-plus" /></a>
-    </div>
-  }  
+
+      {/*
+          <a className="newtab" onClick={this.props.onNewTab}><i className="fa fa-plus" /></a>
+          <a className="close" onClick={this.props.onClose}><i className="fa fa-close" /></a>
+          <a className="minimize" onClick={this.props.onMinimize}><i className="fa fa-circle" /></a>
+          <a className="maximize" onClick={this.props.onMaximize}><i className="fa fa-circle" /></a>
+          */}
+          <a className="app-close" onClick={this.props.onClose}><i className="fa fa-close" /></a>
+      </div>
+  }
 })
