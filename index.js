fe4bae566d9719ee7612afb92fc2d54fa99bfb42
yarec <yarec@163.com>
2016-11-21 13:10:37 +0800

    updater

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const {app, BrowserWindow, globalShortcut, ipcMain } = require('electron');
+var updater = require('electron-updater')
 
 var mainWindow = null
 
@@ -10,14 +11,25 @@ app.on('window-all-closed', function() {
 })
 
 app.on('ready', function () {
-  mainWindow = new BrowserWindow({ width: 1030, height: 920, frame: false })
 
-  // mainWindow.loadURL('file://' + require('path').join(__dirname, 'browser.html'))
-  mainWindow.loadURL('file://' + __dirname + '/browser.html');
-  mainWindow.maximize()
-  mainWindow.on('closed', function() {
-    mainWindow = null
+  updater.on('ready', function () {
+    mainWindow = new BrowserWindow({ width: 1030, height: 920, frame: false })
+
+    // mainWindow.loadURL('file://' + require('path').join(__dirname, 'browser.html'))
+    mainWindow.loadURL('file://' + __dirname + '/browser.html');
+    mainWindow.maximize()
+    mainWindow.on('closed', function() {
+      mainWindow = null
+    })
   })
+  updater.on('updateRequired', function () {
+    app.quit();
+  })
+  updater.on('updateAvailable', function () {
+    mainWindow.webContents.send('update-available');
+  })
+  updater.start()
+
 
   globalShortcut.register('ctrl+shift+1', function () {
     mainWindow.webContents.send('global-shortcut', 0);
