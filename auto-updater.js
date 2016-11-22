0b5150cafaffbb37cfd3bc1e9a56f464475288b0
yarec <yarec@163.com>
2016-11-22 10:00:59 +0800

    auto-updater

---
+++ auto-updater.js
@@ -0,0 +1,56 @@
+const {autoUpdater, ipcRenderer, ipcMain} = require('electron');
+//const ms = require('ms');
+
+const notify = require('./notify'); // eslint-disable-line no-unused-vars
+const {version} = require('./package');
+
+// accepted values: `osx`, `win32`
+// https://nuts.gitbook.com/update-windows.html
+const platform = process.platform === 'darwin' ?
+  'osx' :
+  process.platform;
+const FEED_URL = `http://114.215.168.201:5000/update/${platform}`;
+let isInit = false;
+
+function init() {
+  autoUpdater.on('error', (err, msg) => {
+    console.error('Error fetching updates', msg + ' (' + err.stack + ')');
+  });
+
+  autoUpdater.setFeedURL(`${FEED_URL}/${version}`);
+
+  setTimeout(() => {
+    console.log("try update : " + new Date())
+    autoUpdater.checkForUpdates();
+  }, 1000);
+
+  setInterval(() => {
+    console.log("try update(i) : " + new Date())
+    autoUpdater.checkForUpdates();
+  }, 10000);
+
+  isInit = true;
+}
+
+module.exports = function (win) {
+  if (!isInit) {
+    init();
+  }
+
+  const {rpc} = win;
+
+  const onupdate = (ev, releaseNotes, releaseName) => {
+    console.log("on update")
+    ipcMain.emit('update available', {releaseNotes, releaseName});
+  };
+
+  autoUpdater.on('update-downloaded', onupdate);
+
+  ipcMain.once('quit and install', () => {
+    autoUpdater.quitAndInstall();
+  });
+
+  win.on('close', () => {
+    autoUpdater.removeListener('update-downloaded', onupdate);
+  });
+};
