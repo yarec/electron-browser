fd1265aaee7587a6d119e389910ba98c28791bf2
yarec <yarec@163.com>
2016-11-30 14:42:28 +0800

    makeSingleInstance

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ if (handleSquirrelEvent()) {
 var mainWindow = null
 var settingsWindow = null
 
+const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
+  // Someone tried to run a second instance, we should focus our window.
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) mainWindow.restore()
+    mainWindow.focus()
+  }
+})
+
+if (shouldQuit) {
+  app.quit()
+}
+
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin') {
     app.quit()
